ef2a0ed065e028bb23be8994375eb7bee82c22c3
TheFatAndFurious <mrguerrilla@gmail.com>
2024-02-12 11:04:41 +0100

    the one

---
+++ src/Exemple.java
@@ -0,0 +1,18 @@
+public class Exemple {
+    private String nom;
+    private String prenom;
+
+    public Exemple(){}
+
+    public Exemple(String last_name, String first_name){
+        this.nom = last_name;
+        this.prenom = first_name;
+    }
+    public String get_last_name(){
+        return nom.toUpperCase();
+    }
+
+    public void set_last_name(String last_name) {
+        this.nom = last_name;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Exemple test = new Exemple();
+        Exemple test_2 = new Exemple("Baro", "Mathieu");
+        System.out.println(test_2.nom + " " +  test_2.prenom);
+    }
+}
